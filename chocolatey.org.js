7a6e1e836afa53e043a7ee2de9face0210881369
Markus <nnars@users.noreply.github.com>
2019-01-10 01:40:50 +0100

    Update chocolatey.org.js
    domain matching for Greasemonkey

--- chocolatey.org.js
+++ chocolatey.org.js
@@ -3,26 +3,30 @@
 // Just fun - This script add a button on https://chocolatey.org/packages 
 // to copy the code text in clipboard for easy pasting in command line (ex. Powershell)
 
-const codes = Array.from(document.getElementsByTagName('code'), e => e);
+if (document.location.hostname.match(/chocolatey\.org/)) {
 
-const button = document.createElement('button');
+  const codes = Array.from(document.getElementsByTagName('code'), e => e);
 
-codes.forEach((e, i) => {
-  e.firstElementChild.remove();
-  e.prepend(document.createElement('button'));
-  e.firstElementChild.textContent = 'copy';
-  e.firstElementChild.style.marginRight = '2%';
-  e.firstElementChild.style.border = '0.16em solid #fff';
-  e.firstElementChild.style.borderRadius = '0.4em';
-  e.firstElementChild.style.padding = '0.2em 0.6em';
-  e.firstElementChild.style.boxShadow = '0.12em 0.12em #ccc';
-  e.onclick = () => {
-    const cmdCode = ('' + e.lastChild.textContent).trim() + ' -y -v\n';
+  const button = document.createElement('button');
 
-    navigator.clipboard.writeText(cmdCode).then(function() {
-      /* clipboard successfully set */
-    }, function() {
-      /* clipboard write failed */
-    });
-  };
-});
+  codes.forEach((e, i) => {
+    e.firstElementChild.remove();
+    e.prepend(document.createElement('button'));
+    e.firstElementChild.textContent = 'copy';
+    e.firstElementChild.style.marginRight = '2%';
+    e.firstElementChild.style.border = '0.16em solid #fff';
+    e.firstElementChild.style.borderRadius = '0.4em';
+    e.firstElementChild.style.padding = '0.2em 0.6em';
+    e.firstElementChild.style.boxShadow = '0.12em 0.12em #ccc';
+    e.onclick = () => {
+      const cmdCode = ('' + e.lastChild.textContent).trim() + ' -y -v\n';
+
+      navigator.clipboard.writeText(cmdCode).then(function() {
+        /* clipboard successfully set */
+      }, function() {
+        /* clipboard write failed */
+      });
+    };
+  });
+
+}
