89c6203671e8ebda4fbcd8098806886a3e96bb31
Markus <nnars@users.noreply.github.com>
2019-01-18 14:28:29 +0100

    Update vrealize-operations-manager-login-website.js

--- vrealize-operations-manager-login-website.js
+++ vrealize-operations-manager-login-website.js
@@ -1,23 +1,32 @@
+// ==UserScript==
+// @name     vrealize-operations-manager-login-website.js
+// @version  1
+// @grant    none
+// @author   nnars
+// ==/UserScript==
 'use strict';
 
 // vRealize Operations Manager - Login website
 
 // --- change/customize here --- START --- (examples)
-const hostname = /10\.20\.100\.200/;
+// const hostname = /10\.20\.100\.200/;
 // const hostname = /domain\.tld/;
 // --- change/customize here --- END ---
 
 if (document.location.hostname.match(hostname)) {
 	
 	// --- change/customize here --- START --- (example)
-	const loginUserName = 'domain\\username';
+	// const loginUserName = 'domain\\username';
+  const authSelector = 'All vCenter Servers';
 	// --- change/customize here --- END ---
 	
+  const inputElementAuthSelector = 'authSelector-inputEl';
 	const inputElementUserName = 'userName-inputEl';
 	const inputElementPassword = 'password-inputEl';
 	
 	document.onreadystatechange = function () {
-	    if (document.readyState == 'complete') {
+	    if (document.readyState == 'complete') {        
+				selectAuthSelector(inputElementAuthSelector, authSelector);        
 				insertUsername(inputElementUserName, loginUserName);
 				selectInputElement(inputElementPassword);
 	    }
@@ -33,6 +42,12 @@ if (document.location.hostname.match(hostname)) {
 	const selectInputElement = function (inputElement) {
 		if (document.getElementById(inputElement)) {
 			document.getElementById(inputElement).focus();
+		}
+	};
+	
+	const selectAuthSelector = function (inputElement, textContent) {
+		if (document.getElementById(inputElement)) {
+			document.getElementById(inputElement).value = textContent;
 		}
 	};
 }
