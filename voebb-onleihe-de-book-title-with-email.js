59cea3a6ad714033895d2b07b5abc97b082a2302
Markus <nnars@users.noreply.github.com>
2019-12-03 15:54:34 +0100

    Update voebb-onleihe-de-book-title-with-email.js

--- voebb-onleihe-de-book-title-with-email.js
+++ voebb-onleihe-de-book-title-with-email.js
@@ -1,1 +1,18 @@
-
+'use strict';
+if (document.location.hostname.match(/voebb\.onleihe\.de/)) {
+  document.addEventListener("DOMContentLoaded", function(event) {
+    // --- Edit your e-mail here - START ---
+    const email = 'somebody@somewhere.com';
+    // --- Edit your e-mail here - END ---
+    
+    const inputElementRecipient = document.querySelector("#pRecipient");
+    if (inputElementRecipient) {
+    	inputElementRecipient.value = email;
+    }
+    
+    const inputElementConfirmedRecipient = document.querySelector("#pConfirmedRecipient");
+    if (inputElementConfirmedRecipient) {
+    	inputElementConfirmedRecipient.value = email;
+    }
+  });
+}
