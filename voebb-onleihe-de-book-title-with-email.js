9b7f78245295f758d1ec28c600375bd90584820f
Markus <nnars@users.noreply.github.com>
2020-05-14 14:34:40 +0200

    looks better minimal

--- voebb-onleihe-de-book-title-with-email.js
+++ voebb-onleihe-de-book-title-with-email.js
@@ -1,6 +1,7 @@
 'use strict';
 if (document.location.hostname.match(/voebb\.onleihe\.de/)) {
   document.addEventListener("DOMContentLoaded", function(event) {
+    
     // --- Edit your e-mail here - START ---
     const email = 'somebody@somewhere.com';
     // --- Edit your e-mail here - END ---
@@ -9,6 +10,20 @@ if (document.location.hostname.match(/voebb\.onleihe\.de/)) {
       document.querySelector("#header-banner-nav > ul").style.display = 'none';
       document.querySelector("#header-banner-nav").style.height = '2.3rem';
       document.querySelector("#main > div.pagebox > footer").style.display = 'none';
+    */
+
+    /*
+      var headerEl = document.querySelector("#main > header > div > div.row-4 > div > div");
+      if (headerEl) { headerEl.style.height = '25px'; }
+
+      var headerBanner = document.querySelector("#header-banner-nav > ul");
+      if (headerBanner) { headerBanner.style.height = '0'; }
+
+      var footerEl = document.querySelector("footer")
+      if (footerEl) {
+        footerEl.style.height = '0';
+        footerEl.style.display = 'none';
+      }
     */
 
     const inputElementRecipient = document.querySelector("#pRecipient");
@@ -20,5 +35,6 @@ if (document.location.hostname.match(/voebb\.onleihe\.de/)) {
     if (inputElementConfirmedRecipient) {
     	inputElementConfirmedRecipient.value = email;
     }
+    
   });
 }
