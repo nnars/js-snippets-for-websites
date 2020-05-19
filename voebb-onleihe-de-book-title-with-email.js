9c8a46cb4d42052b8efc4926211d53ea26de9922
Markus <nnars@users.noreply.github.com>
2020-05-19 16:12:56 +0200

    added some checks

--- voebb-onleihe-de-book-title-with-email.js
+++ voebb-onleihe-de-book-title-with-email.js
@@ -6,6 +6,17 @@ if (document.location.hostname.match(/voebb\.onleihe\.de/)) {
     const email = 'somebody@somewhere.com';
     // --- Edit your e-mail here - END ---
 
+    const inputElementRecipient = document.querySelector("#pRecipient");
+    if (inputElementRecipient) {
+    	inputElementRecipient.value = email;
+    }
+    
+    const inputElementConfirmedRecipient = document.querySelector("#pConfirmedRecipient");
+    if (inputElementConfirmedRecipient) {
+    	inputElementConfirmedRecipient.value = email;
+    }
+    
+    
     /*
       document.querySelector("#header-banner-nav > ul").style.display = 'none';
       document.querySelector("#header-banner-nav").style.height = '2.3rem';
@@ -25,58 +36,46 @@ if (document.location.hostname.match(/voebb\.onleihe\.de/)) {
         footerEl.style.display = 'none';
       }
     */
-
-    const inputElementRecipient = document.querySelector("#pRecipient");
-    if (inputElementRecipient) {
-    	inputElementRecipient.value = email;
-    }
-    
-    const inputElementConfirmedRecipient = document.querySelector("#pConfirmedRecipient");
-    if (inputElementConfirmedRecipient) {
-    	inputElementConfirmedRecipient.value = email;
-    }
-    
     
     /*
     var article_info = document.querySelector("#main > div.pagebox > div > main > div > div.row-2 > div > article > div.article-info > div.article-info-top");
+    if (article_info) {
 
-    var bookTitleEl = document.querySelector("#main > div.pagebox > div > main > div > div.row-2 > div > article > div.article-info > div.article-info-top > div.title-name");
-    var sBookTitle = bookTitleEl.lastChild.textContent.trim();
+        var bookTitleEl = document.querySelector("#main > div.pagebox > div > main > div > div.row-2 > div > article > div.article-info > div.article-info-top > div.title-name");
+        var sBookTitle = '';
+        if (true) {
+            sBookTitle = bookTitleEl.lastChild.textContent.trim();
+        }
 
-    var bookSubtitleEl = document.querySelector("#main > div.pagebox > div > main > div > div.row-2 > div > article > div.article-info > div.article-info-top > div.subtitle");
-    var sbookSubtitle = bookSubtitleEl.lastChild.textContent.trim();
+        var bookSubtitleEl = document.querySelector("#main > div.pagebox > div > main > div > div.row-2 > div > article > div.article-info > div.article-info-top > div.subtitle");
+        var sbookSubtitle = '';
+        if (true) {
+            sbookSubtitle = publishingDateEl.lastChild.textContent.trim();
+        }
 
-    var publishingDateEl = document.querySelector("#main > div.pagebox > div > main > div > div.row-2 > div > article > div.article-info > div.article-info-top > div.publishing-date");
-    var spublishingDate = publishingDateEl.lastChild.textContent.trim();
+        var publishingDateEl = document.querySelector("#main > div.pagebox > div > main > div > div.row-2 > div > article > div.article-info > div.article-info-top > div.publishing-date");
+        var spublishingDate = '';
+        if (true) {
+            spublishingDate = publishingDateEl.lastChild.textContent.trim();
+        }
 
-    var copyTitleBtn = document.createElement("button");
-    copyTitleBtn.textContent = 'copy title';
-    copyTitleBtn.style.border = '2px solid black';
-    copyTitleBtn.style.margin = '0 5px 0 0';
-    copyTitleBtn.style.backgroundColor = 'antiquewhite';
-    copyTitleBtn.style.padding = '4px 8px';
-    copyTitleBtn.style.fontFamily = 'monospace';
-    copyTitleBtn.style.borderRadius = '8px';
-    copyTitleBtn.style.fontWeight = 'bold';
-    copyTitleBtn.value = sBookTitle + ' (' + spublishingDate + ') ' + sbookSubtitle;
-    copyTitleBtn.addEventListener('click', fCopyTitle);
 
-    function fCopyTitle() { 
+        var copyTitleBtn = document.createElement("button");
+        copyTitleBtn.textContent = 'copy title';
+        copyTitleBtn.style.border = '2px solid black';
+        copyTitleBtn.style.margin = '0 5px 0 0';
+        copyTitleBtn.style.backgroundColor = 'antiquewhite';
+        copyTitleBtn.style.padding = '4px 8px';
+        copyTitleBtn.style.fontFamily = 'monospace';
+        copyTitleBtn.style.borderRadius = '8px';
+        copyTitleBtn.style.fontWeight = 'bold';
+        copyTitleBtn.value = sBookTitle + ' (' + spublishingDate + ') ' + sbookSubtitle;
+        copyTitleBtn.addEventListener('click', fCopyTitle);
 
-      var bookFullTitle = sBookTitle + ' (' + spublishingDate + ') ' + sbookSubtitle;
-      updateClipboard(bookFullTitle);
+
+        article_info.prepend(copyTitleBtn);
 
     }
-
-    function updateClipboard(newClip) {
-      navigator.clipboard.writeText(newClip).then(function() {
-        // clipboard successfully set
-      }, function() {
-        // clipboard write failed
-      });
-    }
-
-    article_info.prepend(copyTitleBtn);
     */    
     
   });
